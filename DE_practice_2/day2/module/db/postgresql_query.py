f2f8026fd788435464a2a7b07dc4751e874806be
dnddl6962 <77288155+dnddl6962@users.noreply.github.com>
2024-03-05 05:17:52 +0900

    DE_BASIC

---
+++ DE_practice_2/day2/module/db/postgresql_query.py
@@ -0,0 +1,10 @@
+## POSTGRESQL : postgres database
+
+queries = {
+    "point" : "SELECT * FROM point WHERE credate = '{batch_date}'",
+    "lecture" : "SELECT * FROM lecture",
+    # "lecture01" : "SELECT * FROM lecture01 WHERE credate = '{batch_date}'",
+    # "lecture02" :   "SELECT * FROM lecture02",
+    # "taeyoung" :   "SELECT * FROM taeyoung"
+}    
+
