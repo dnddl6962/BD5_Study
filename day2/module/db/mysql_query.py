c2975ab82e96d1bdddd63e0146ccf27ac8675527
dnddl6962 <77288155+dnddl6962@users.noreply.github.com>
2024-03-05 05:22:09 +0900

    Delete day2/module directory

--- day2/module/db/mysql_query.py
+++
@@ -1,9 +0,0 @@
-## MYSQL : learner_choi database
-
-queries = {
-    "item_his" : "SELECT * FROM item_his WHERE proc_ymd = '{batch_date}'",
-    "point_his" :   "SELECT * FROM point_his WHERE proc_ymd = '{batch_date}'",
-    "regdate_his" : "SELECT * FROM regdate_his WHERE regdate = '{batch_date}'",
-    "study_his" :   "SELECT * FROM study_his WHERE proc_ymd = '{batch_date}'",
-    "member" :   "SELECT * FROM member"
-}
--- day2/module/pipeline/remove.py
+++
@@ -1,8 +0,0 @@
-import shutil, os
-from settings import TEMP_PATH
-
-def remover(TEMP_PATH):
-    
-    shutil.rmtree(TEMP_PATH)
-
-    os.makedirs(TEMP_PATH)
